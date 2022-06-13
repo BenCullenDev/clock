4613169b297d5736e184bb61d619e10f97856868
Ben Cullen <benjaminjcullen1@gmail.com>
2022-06-13 11:31:40 +0100

    basic clock that updates every second

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Clock extends React.Component {
+  state = {time: null}
+  componentDidMount() {
+      setInterval(() => {
+          this.setState({time: new Date().toLocaleTimeString()})   
+      }, 1000)
+  }
+  
+  render() {
+      return (
+          <div className="time">
+              The time is: {this.state.time}
+          </div>
+      );
+  }
+}
+ReactDOM.render(
+    <Clock />,
+    document.querySelector('#root')
+)
